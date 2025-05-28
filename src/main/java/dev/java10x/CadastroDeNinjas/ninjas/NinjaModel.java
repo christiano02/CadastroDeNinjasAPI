352e976bc061746168b361d92b9ac81d6b41dd1b
Christiano <christianonascimento02@hotmail.com>
2025-05-27 22:01:28 -0300

    fix: adicionando @Column e um novo atributo na classe NinjaModel

--- src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaModel.java
@@ -16,13 +16,19 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
     @Column(unique = true)
     private String email;
 
+    @Column(name="imgUrl")
+    private String imgUrl;
+
+    @Column(name = "age")
     private int age;
 
     //ManyToOne um ninja tem uma unica missão
