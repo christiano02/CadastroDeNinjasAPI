4194b92c8118920631eaac976ce3c1f3dfe1024b
Christiano <christianonascimento02@hotmail.com>
2025-08-18 22:23:22 -0300

    fix: removendo o loombok e criando manualmente os construtores e os getters e setters

--- src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaModel.java
@@ -8,12 +8,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 
 @Entity
 @Table(name="tb_cadastro")
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class NinjaModel {
 
     @Id
@@ -38,4 +37,74 @@ public class NinjaModel {
     @JoinColumn(name="missoes_id") //foreing key ou chave estrangeira
     private MissoesModel missoes;
 
+    public NinjaModel(Long id, String name, String email, String imgUrl, int age, MissoesModel missoes) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.imgUrl = imgUrl;
+        this.age = age;
+        this.missoes = missoes;
+    }
+    public NinjaModel(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public MissoesModel getMissoes() {
+        return missoes;
+    }
+
+    public void setMissoes(MissoesModel missoes) {
+        this.missoes = missoes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NinjaModel that = (NinjaModel) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
