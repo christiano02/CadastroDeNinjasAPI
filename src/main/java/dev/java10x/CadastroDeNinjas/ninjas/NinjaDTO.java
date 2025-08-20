ca3dd4ae531296f2915c3f7cd6f41867d0630050
Christiano <christianonascimento02@hotmail.com>
2025-08-20 20:43:55 -0300

    Chore: inclui a versão do DTO pela entidade e mapeia manualmente no ninjaMapper os caminhos entre entidade e DTO

--- src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaDTO.java
@@ -1,4 +1,85 @@
 package dev.java10x.CadastroDeNinjas.ninjas;
 
-public class NinjaDTO {
+
+import dev.java10x.CadastroDeNinjas.missoes.MissoesModel;
+
+public class NinjaDTO{
+
+    private Long id;
+    private String name;
+    private String email;
+    private String imgUrl;
+    private int age;
+    private String rank;
+    private MissoesModel missoes;
+
+    public NinjaDTO(){}
+
+    public NinjaDTO(Long id, String name, String email, String imgUrl, int age, MissoesModel missoes, String rank) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.imgUrl = imgUrl;
+        this.age = age;
+        this.missoes = missoes;
+        this.rank = rank;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public MissoesModel getMissoes() {
+        return missoes;
+    }
+
+    public void setMissoes(MissoesModel missoes) {
+        this.missoes = missoes;
+    }
+
+
 }
